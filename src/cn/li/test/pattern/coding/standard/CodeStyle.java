820bf883593a5f6d71078d14e7809ec7e3dc33b3
lihaining <760865548@qq.com>
2016-06-14 18:17:06 +0800

    # WARNING: head commit changed in the meantime
    简单工厂模式

---
+++ src/cn/li/test/pattern/coding/standard/CodeStyle.java
@@ -0,0 +1,72 @@
+package cn.li.test.pattern.coding.standard;
+
+import java.util.Scanner;
+
+//public
+class Calculator {
+	public static void startCalculator1(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("请输入数字a:");
+		Float a = sc.nextFloat();
+		System.out.println("请输入运算符号（+、-、*、/）:");
+		String b = sc.next();
+		System.out.println("请输入数字c:");
+		float c = sc.nextFloat();
+		
+		float d = 0f;
+		
+		if(b.equals("+")){
+			d = a + c;
+		}
+		if(b.equals("-")){
+			d = a - c;
+		}
+		if(b.equals("*")){
+			d = a * c;
+		}
+		if(b.equals("/")){
+			d = a / c;
+		}
+		System.out.println("计算结果：" + d);
+		
+		sc.close();
+	}
+	
+	public static void startCalculator2(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("请输入数字a:");
+		float numberA = sc.nextFloat();
+		System.out.println("请输入运算符号（+、-、*、/）:");
+		String strOperate = sc.next();
+		System.out.println("请输入数字b:");
+		float numberB = sc.nextFloat();
+		
+		float val = 0f;
+		
+		switch (strOperate){
+			case "+":
+				val = numberA + numberB;
+				break;
+			case "-":
+				val = numberA - numberB;
+				break;
+			case "*":
+				val = numberA * numberB;
+				break;
+			case "/":
+				if(numberB == 0){
+					System.out.println("除数不能为0");
+					break;
+				}
+				val = numberA / numberB;
+		}
+		System.out.println("计算结果：" + val);
+		
+		sc.close();
+	}
+}
+public class CodeStyle {
+	public static void main(String[] args) {
+		Calculator.startCalculator2();
+	}
+}
